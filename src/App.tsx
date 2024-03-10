02af5b819d4ec8fc1fca4e19a22ec1333b3604a7
Farhat Baaroun <69281333+GldzzPro@users.noreply.github.com>
2024-03-10 16:42:45 +0100

    init folder

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { Data } from "./types";
+import { EngineRecap } from "./components/EngineRecap/EngineRecap";
+import { GeneralStatusItem } from "./components/GeneralStatusItem/GeneralStatusItem";
+import { DateInfo } from "./components/DateInfo/DateInfo";
+import { OtherInfo } from "./components/OtherInfo/OtherInfo";
+import { ConfigSection } from "./components/ConfigSection/ConfigSection";
+import { Title } from "./components/Title/Title";
+import { GENERAL_STATUS_TITLE } from "./const";
+
+function App() {
+  const [data, setData] = useState<Data>({
+    engines: [],
+  });
+  useEffect(() => {
+    const fetchData = async () => {
+      fetch("http://localhost:5173/data.json")
+        .then((response) => response.json())
+        .then((data) => setData(data));
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div style={{ display: "flex", flexDirection: "row", width: "100vw" }}>
+      {data.engines.length > 1 && (
+        <>
+          <EngineRecap data={data.engines[0]} />
+          <div style={{ display: "flex", flexDirection: "column" , justifyContent : "space-between"  , flex : 1 ,  padding : "5px" , border : "1px solid black"}}>
+            <Title title={GENERAL_STATUS_TITLE} />
+            <DateInfo date={new Date()} />
+            <GeneralStatusItem name={data.engines[0]?.name} status="normal" />
+            <GeneralStatusItem name={data.engines[1]?.name} status="normal" />
+            <OtherInfo
+              firstEngine={data.engines[0]}
+              secondEngine={data.engines[1]}
+            />
+            <ConfigSection />
+          </div>
+          <EngineRecap data={data.engines[1]} />
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
